08f8ac242fc2f0039ebe2fb21192371837c10e66
Steve Pentler <stevepentler@gmail.com>
2015-12-26 10:16:07 -0600

    create playlist error

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151223234638) do
+ActiveRecord::Schema.define(version: 20151226155254) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,6 +19,12 @@ ActiveRecord::Schema.define(version: 20151223234638) do
   create_table "artists", force: :cascade do |t|
     t.string   "name"
     t.string   "image_path"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "playlists", force: :cascade do |t|
+    t.string   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
